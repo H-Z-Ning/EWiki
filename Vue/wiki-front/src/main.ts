8c8db1d2251b355635fb28d7c42f9d063be5ed52
H-Z-Ning <zhining1024@163.com>
2025-10-10 16:12:58 +0800

    Update main.ts

--- Vue/wiki-front/src/main.ts
+++ Vue/wiki-front/src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import i18n from './i18n'
+const app = createApp(App)
 
-createApp(App).use(router).mount('#app')
+app.use(router)
+app.use(i18n)
+app.mount('#app')
